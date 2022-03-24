55add0d0fd639a99b5efea03bc7d06996fdc19a3
Gabriel Anatrone <anatrone.gabriel@gmail.com>
2022-03-23 22:09:19 -0300

    test: ensure EmailValidatorAdapter call validator with correct email

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -22,4 +22,12 @@ describe('EmailValidator Adapter', () => {
 
     expect(isValid).toBe(true)
   })
+
+  test('should call validator with correct email', () => {
+    const sut = new EmailValidatorAdapter()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    const isValid = sut.isValid('any_email@mail.com')
+
+    expect(isEmailSpy).toBeCalledWith('any_email@mail.com')
+  })
 })
