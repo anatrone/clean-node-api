d93a82185a83c88b0502c557c7289dfdd91530da
Gabriel Anatrone <anatrone.gabriel@gmail.com>
2022-03-30 18:58:57 -0300

    refactor: move mapping to MongoHelper

--- src/infrastructure/db/mongodb/helpers/mongo-helper.ts
+++ src/infrastructure/db/mongodb/helpers/mongo-helper.ts
@@ -18,5 +18,10 @@ export const MongoHelper = {
 
   getCollection (name: string): Collection {
     return this.client.db().collection(name)
+  },
+
+  map: (collection: any): any => {
+    const { _id, ...collectionWithId } = collection
+    return Object.assign({}, collectionWithId, { id: _id })
   }
 }
