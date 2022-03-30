a9a371f8eb68832dcb697b404158951b49c0b91f
Gabriel Anatrone <anatrone.gabriel@gmail.com>
2022-03-30 20:15:48 -0300

    feat: ensure cors is enable

--- src/main/middlewares/body-parser.test.ts
+++ src/main/middlewares/body-parser.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import app from '../config/app'
 
 describe('Body Parser Middleware', () => {
-  test('should ', async () => {
+  test('should parse bodyu as json', async () => {
     app.post('/test_body_parser', (req, res) => {
       res.send(req.body)
     })
---
+++ src/main/middlewares/cors.test.ts
@@ -0,0 +1,14 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('CORS Middleware', () => {
+  test('should enable cors', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send()
+    })
+
+    await request(app)
+      .get('/test_body_parser')
+      .expect('access-control-allow-origin', '*')
+  })
+})
