18edbcecedb2601d50884bfccc2044f11f075957
Gabriel Anatrone <anatrone.gabriel@gmail.com>
2023-06-28 17:58:09 -0300

    feat: ensure SignUpController calls validation if correct value

--- src/presentation/controllers/signup/signup-protocols.ts
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -1,4 +1,5 @@
 export * from '../../protocols'
 export * from '../../protocols/emailValidator'
+export * from '@/presentation/helpers/validators/validation'
 export * from '../../../domain/usecases/add-account'
 export * from '../../../domain/models/account'
--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,18 +1,21 @@
-import { HttpRequest, HttpResponse, Controller, EmailValidator, AddAccount } from './signup-protocols'
+import { HttpRequest, HttpResponse, Controller, EmailValidator, AddAccount, Validation } from './signup-protocols'
 import { MissingParamError, InvalidParamError } from '../../errors'
 import { badRequest, serverError, successRequest } from '../../helpers/http-helper'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
   private readonly addAccount: AddAccount
+  private readonly validation: Validation
 
-  constructor (emailValidatorValue: EmailValidator, addAccount: AddAccount) {
+  constructor (emailValidatorValue: EmailValidator, addAccount: AddAccount, validation: Validation) {
     this.emailValidator = emailValidatorValue
     this.addAccount = addAccount
+    this.validation = validation
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.validation.validate(httpRequest.body)
       const { name, email, password, passwordConfirmation } = httpRequest.body
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
